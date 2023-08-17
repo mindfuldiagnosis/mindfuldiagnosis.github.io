7323088d2d301a5d8f1da830aaeada0b0521c92d
Ayyoub Abdel-Aziz <ayyoub2503@gmail.com>
2023-08-17 11:24:01 -0400

    bleh

--- js/teamscroll.js
+++ js/teamscroll.js
@@ -1,17 +1,15 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-  
-  window.addEventListener("scroll", reveal);
+// Initialize ScrollReveal
+const sr = ScrollReveal({
+  distance: '30px',
+  duration: 1000,
+  easing: 'ease',
+  mobile: true,
+});
+
+// Define the elements you want to reveal
+const teamMembers = document.querySelectorAll('.hero');
+
+// Apply the reveal animation to each team member section
+teamMembers.forEach((member) => {
+  sr.reveal(member);
+});
